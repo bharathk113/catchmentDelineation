a4aea194273850e77a150afe4a8d5c27a373f06f
Bharath Reddy <bharathkadapala@gmail.com>
2019-04-18 23:56:46 +0530

    For single point-with numba

--- code/catchment.py
+++ code/catchment.py
@@ -1,9 +1,12 @@
 import gdal
-import numpy
+import numpy,numba
 from os import listdir,path,walk,system,sys
 from math import ceil,floor
+import time
+from numba import jit
 """
     77.26744369,20.06364357
+    Requires above mentioned packages
 """
 def readReleventArray(raster,gt,point):
     nB=gt[3]
@@ -23,32 +26,91 @@ def readReleventArray(raster,gt,point):
     pointPixel=[floor((point[0]-gt[0])/gt[1]),floor((point[1]-gt[3])/gt[5])]
     # print arrayBounds[0],arrayBounds[1],arrayBounds[2]-arrayBounds[0],arrayBounds[3]-arrayBounds[1]
     print pointPixel
-    releventArray=raster.ReadAsArray(arrayBounds[0],arrayBounds[1],arrayBounds[2]-arrayBounds[0],arrayBounds[3]-arrayBounds[1])
+    releventArray=raster.ReadAsArray(arrayBounds[0],arrayBounds[1],arrayBounds[2]-arrayBounds[0]-1,arrayBounds[3]-arrayBounds[1]-1)
+    # print releventArray
     return releventArray,arrayBounds,pointPixel
-def Core(array,point):
-    outArray=numpy.zeros()
+@jit(nopython=True,nogil=True)
+def Core(inArray,point):
+    point=[int(point[1]),int(point[0])]
+    outArray=numpy.zeros(inArray.shape)
     toChklist=[point]
-    addedList=[point]
-    for eachPoint in toChklist:
-        for i in [(1,1),(1,0),(1,-1),(0,1),(0,-1),(-1,-1),(-1,0),(-1,1)]:
-            print array[int(point[1]+i[1]),int(point[0]+i[0])],i
+    iter=0
+    while iter<len(toChklist):
+    # for eachPoint in toChklist:
+        eachPoint=toChklist[iter]
+        # if eachPoint!=None:
+        #     continue
+        # print eachPoint
+        # for i in [(1,1),(1,0),(1,-1),(0,1),(0,-1),(-1,-1),(-1,0),(-1,1)]:
+        outArray[int(eachPoint[0]),int(eachPoint[1])]=1
+        if inArray[eachPoint[0]+1,eachPoint[1]+1]==3:
+            outArray[eachPoint[0]+1,eachPoint[1]+1]=1
+            toChklist.append([eachPoint[0]+1,eachPoint[1]+1])
+        if inArray[eachPoint[0]+1,eachPoint[1]]==2:
+            outArray[eachPoint[0]+1,eachPoint[1]]=1
+            toChklist.append([eachPoint[0]+1,eachPoint[1]])
+        if inArray[eachPoint[0]+1,eachPoint[1]-1]==1:
+            outArray[eachPoint[0]+1,eachPoint[1]-1]=1
+            toChklist.append([eachPoint[0]+1,eachPoint[1]-1])
+        if inArray[eachPoint[0],eachPoint[1]+1]==4:
+            outArray[eachPoint[0],eachPoint[1]+1]=1
+            toChklist.append([eachPoint[0],eachPoint[1]+1])
+        if inArray[eachPoint[0],eachPoint[1]-1]==8:
+            outArray[eachPoint[0],eachPoint[1]-1]=1
+            toChklist.append([eachPoint[0],eachPoint[1]-1])
+        if inArray[eachPoint[0]-1,eachPoint[1]+1]==5:
+            outArray[eachPoint[0]-1,eachPoint[1]+1]=1
+            toChklist.append([eachPoint[0]-1,eachPoint[1]+1])
+        if inArray[eachPoint[0]-1,eachPoint[1]]==6:
+            outArray[eachPoint[0]-1,eachPoint[1]]=1
+            toChklist.append([eachPoint[0]-1,eachPoint[1]])
+        if inArray[eachPoint[0]-1,eachPoint[1]-1]==7:
+            outArray[eachPoint[0]-1,eachPoint[1]-1]=1
+            toChklist.append([eachPoint[0]-1,eachPoint[1]-1])
+        # print inArray[eachPoint[0]+1,eachPoint[1]+1]
+        # print inArray[eachPoint[0]+1,eachPoint[1]]
+        # print inArray[eachPoint[0]+1,eachPoint[1]-1]
+        # print inArray[eachPoint[0],eachPoint[1]+1]
+        # print inArray[eachPoint[0],eachPoint[1]-1]
+        # print inArray[eachPoint[0]-1,eachPoint[1]+1]
+        # print inArray[eachPoint[0]-1,eachPoint[1]]
+        # print inArray[eachPoint[0]-1,eachPoint[1]-1]
+        # print len(toChklist)
+        # raw_input("Test")
+        iter+=1
+        # print len(toChklist),toChklist,eachPoint,iter
         # toChklist.remove(eachPoint)
+        # toChklist=toChklist[1:]
+    return outArray
+    # print toChklist
     # while True:
     #     pixelsIn=len(addedList)
     #     for eachPoint in toChklist:
     #         for i in range(0,8):
     #             array[point[1],point[0]]
     #         toChklist.remove(eachPoint)
-def getCatchment(gt,array,arrayBounds,pointPixel):
+def getCatchment(gt,relArray,arrayBounds,pointPixel,outFile,proj):
     arrayTopCords=[gt[0]+arrayBounds[0]*gt[1],gt[3]+arrayBounds[1]*gt[5]]
     pointPixel=[pointPixel[0]-arrayBounds[0],pointPixel[1]-arrayBounds[1]]
-    Core(array,pointPixel)
-flowDirFile="D:\NHP\dev_catchment\data\\testDir.tif"
-segFile="D:\NHP\dev_catchment\data\\testSeg.tif"
-point=(77.2735235,19.9672154)
-compBuf=0.1
+    outArray=Core(relArray,pointPixel)
+    driver = gdal.GetDriverByName('GTIFF')
+    outRaster = driver.Create(outFile, relArray.shape[1],relArray.shape[0], 1, gdal.GDT_Byte, ['NBITS=1'])
+    outRaster.SetGeoTransform([arrayTopCords[0],gt[1],gt[2],arrayTopCords[1],gt[4],gt[5]])
+    outRaster.SetProjection(proj)
+    outband = outRaster.GetRasterBand(1)
+    outband.SetNoDataValue(0)
+    outband.WriteArray(outArray)
+start = time. time()
+flowDirFile="/home/bharath/Documents/catchmentDelineation/data/testDir.tif"
+outFile="/home/bharath/Documents/catchmentDelineation/data/testWSnumba.tif"
+# segFile="D:\NHP\dev_catchment\data\\testSeg.tif"
+point=(77.20723842,19.95570563)
+compBuf=1
 raster=gdal.Open(flowDirFile)
 gt=raster.GetGeoTransform()
+proj=raster.GetProjection()
 relArray,arrayBounds,pointPixel=readReleventArray(raster,gt,point)
 # print (gdal.Open(segFile)).ReadAsArray(int(pointPixel[0]),int(pointPixel[1]),1,1)
-getCatchment(gt,array,arrayBounds,pointPixel)
+getCatchment(gt,relArray,arrayBounds,pointPixel,outFile,proj)
+end = time. time()
+print(end - start)
